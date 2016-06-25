a40d79450c8f8431a42c1ab105bced0cd07c536b
Sugarblade <William.f.skelton@outlook.com>
2016-06-25 10:03:13 -0700

    Constructed the picture objects

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
+/* ============= Functions: ============= */
+function specificRandom(min, max){
+	return Math.random() * (max-min) + min;
+}
+
 /* ============= Constructor: ============= */
 
-function Picture(name, fileType) {
+function Picture(name) {
 	this.name = name;
-	this.path = "assets/" + name + "." + fileType;
+	this.path = "assets/" + name + ".jpg";
+	this.timesChosen = 0;
+	allPictures.push(this);
 }
 
 /* ============= Respective Methods: ============= */
 
 /* ============= Global Variables: ============= */
-var picArray = [];
-
-/* ============= Functions: ============= */
-
+var allPictures = [];
+var webSpot = ["plcHldr1", "plcHldr2", "plcHldr3"];
+var picArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 /* ============= Main(): ============= */
 
-var Bag = new Picture("bag", "jpg");
-console.log(Bag);
 
-var parent = document.getElementById('pic1');
-var child = document.createElement('img');
-child.src = Bag.path;
-parent.appendChild(child);
+//Constructs
+for (var i = 0; i < picArray.length; i++) {
+	var pic = new Picture(picArray[i]);
+}
+
+
+
+
+/* ============= For Later: ============= */
+// var Bag = new Picture("bag", "jpg");
+// console.log(Bag);
+//
+// var parent = document.getElementById('pic1');
+// var child = document.createElement('img');
+// child.src = Bag.path;
+// parent.appendChild(child);
 
 
 
